f84837c01f2fff2c69b574644537439f39972c7d
Lithemba <4530588@myuwc.ac.za>
2026-04-11 01:04:36 +0200

    Binary sort added as well, almost done

--- Shuffling.java/src/YatesShuffling.java
+++ Shuffling.java/src/YatesShuffling.java
@@ -27,5 +27,20 @@ public class Chatgpt {
         }
         return arr;
     }
+    public static int sort(int[] arr,int low,int high,int target){
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] < target){
+                low = mid +1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return -1;
+    }
     
 }
