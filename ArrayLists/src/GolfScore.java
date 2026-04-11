dd98d97b48d436ce3b281a9c202357cdec4138ba
Lithemba <4530588@myuwc.ac.za>
2026-04-11 15:29:50 +0200

    100% better version of the code and much efficient

--- ArrayLists/src/GolfScore.java
+++ ArrayLists/src/GolfScore.java
@@ -1,6 +1,6 @@
 /** Program that reads Golf scores of a group of athletes, computes the average score, and shows how each score
  * differs from the average score **/
-
+import java.lang.*;
 import java.util.*;
 public class GolfScore {
     public static void fillScores(ArrayList<Double> arr){
@@ -25,14 +25,13 @@ public class GolfScore {
     public static void difference(ArrayList<Double> arr){
         double average = average(arr);
         for(double a: arr){
-            System.out.println(a+" differs from the average "+average+" by "+(a - average));
+            System.out.println(a+" differs from the average "+average+" by "+Math.abs((a - average)));
         }
     }
     public static void main(String[] args){
         ArrayList<Double> scores = new ArrayList<>();
         System.out.println( "Program simply takes scores, computes average and shows how each score differs from the average score :");
         System.out.println(" ");
-        System.out.println("Enter golf scores : ");
         fillScores(scores);
         difference(scores);
 
