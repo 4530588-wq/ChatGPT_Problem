787b06e3577c920738deb07411802cedbbb0e528
Lithemba <4530588@myuwc.ac.za>
2026-04-11 14:15:43 +0200

    Created my arraylist and declared some variables

---
+++ ArrayLists/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        ArrayList<String> groceryList = new ArrayList<>(10);
+        Scanner scan = new Scanner(System.in);
+        boolean done = false;
+        String next = null;
+        String answer;
+        
+    }
+}
