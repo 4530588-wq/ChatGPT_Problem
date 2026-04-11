719ae4f91f6a43cc7deec9afec79fe18e2fefdba
Lithemba <4530588@myuwc.ac.za>
2026-04-11 16:04:02 +0200

    Last method added

--- ArrayLists/src/SortingArrayList.java
+++ ArrayLists/src/SortingArrayList.java
@@ -1,7 +1,5 @@
 import java.util.*;
 public class SortingArrayList {
-
-
     public static void sort(ArrayList<Integer> arr) {
         int n = arr.size();
         for (int i = 1; i < n; i++) {
@@ -15,5 +13,20 @@ public class SortingArrayList {
         }
         System.out.println("Sorted array :\n " + arr.toString());
     }
-    
+    public static void fillList(ArrayList<Integer> arr){
+        Scanner scan = new Scanner(System.in);
+        int next;
+        System.out.print("Enter ages , enter negative age to stop : ");
+        next = scan.nextInt();
+        while(next >= 0){
+            arr.add(next);
+            next = scan.nextInt();
+        }
+    }
+    public static void main(String[] args){
+        ArrayList<Integer> ages = new ArrayList<>();
+        System.out.println("Results : ");
+        fillList(ages);
+        sort(ages);
+    }
 }
